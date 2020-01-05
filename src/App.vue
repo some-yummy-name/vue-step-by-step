0837d87c234e1fb4c5c26c94c931f8e1d2810634
Marat <yeffasol@mail.ru>
2020-01-05 14:42:01 +0300

    Add first component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <input type="text" v-model="message">
+        <Task>name</Task>
+        <Task>another</Task>
+        <div>{{message}}</div>
+        <ul>
+            <li v-for="(item,index) in list" :key="index">{{item}}</li>
+        </ul>
+        <ul>
+            <li v-for="(item,index) in upperList" :key="index">{{item}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Task from "./components/Task"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			Task
+        },
+		data() {
+			return {
+				message: "",
+				list: ["one", "two"]
+			}
+		},
+		computed: {
+			upperList() {
+				return this.list.map(item => item.toUpperCase());
+			}
+
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
